87bab78e71a6088f71a811d84633fdcc5e7a1151
Ville Vainio <ville.vainio@gmail.com>
2018-11-25 16:06:51 +0200

    Add ability to select playable video

---
+++ src/components/VideoDetail.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <template v-if="video">
+      <iframe :src="videoUrl" />
+      <div>
+        <div>{{ video.snippet.title }}</div>
+        <div>{{ video.snippet.description }}</div>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+const BASE_URL = 'https://www.youtube.com/embed/'
+
+export default {
+  name: 'VideoDetail',
+
+  props: {
+    video: {
+      type: Object,
+      default: null
+    }
+  },
+
+  computed: {
+    videoUrl () {
+      const { videoId } = this.video.id
+      return `${BASE_URL}/${videoId}`
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
