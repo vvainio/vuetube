302b05f28b151496f09fa34d8237acdbc51b9ce9
Ville Vainio <ville.vainio@gmail.com>
2018-11-25 15:29:26 +0200

    Add ability to search for videos

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,36 @@
+<template>
+  <input
+    v-model.trim="searchTerm"
+    type="search"
+    placeholder="Search YouTube"
+    @keydown.enter="onEnter"
+  >
+</template>
+
+<script>
+export default {
+  name: 'SearchInput',
+
+  props: {
+    searchFn: {
+      type: Function,
+      required: true
+    }
+  },
+
+  data: () => ({
+    searchTerm: ''
+  }),
+
+  methods: {
+    onEnter () {
+      if (this.searchTerm) {
+        this.searchFn(this.searchTerm)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
